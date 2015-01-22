80de5f852556ab2eb276d48d500e40fa880ff10f
flower-pot <fbranczyk@gmail.com>
2015-01-22 16:10:58 +0100

    add prefix parameter
    closes #2

--- cli.js
+++ cli.js
@@ -3,11 +3,9 @@ Error.stackTraceLimit = Infinity;
 
 var program = require('commander');
 var fs = require('fs');
-var os = require("os");
+var os = require('os');
 var Instrumenter = require('./instrumenter');
 var package = require('./package.json');
-
-const PREFIX = "instrumented-";
 
 program
   .version(package.version)
@@ -19,7 +17,8 @@ program
   .option('-v, --verbose', 'Display some information on the current status')
   .option('-q, --quiet', 'Surpress warnings and log output')
   .option('-p, --parallelism <forks>', 'Spread work over n parallel processes (defaults to amount of available cpu cores)', os.cpus().length)
-  .option("--cleanup", "Removes all files in the given targets starting with '" + PREFIX + "'");  
+  .option('--prefix [prefix]', 'The prefix to use to indicate a file is instrumented (by default "instrumented-")', 'instrumented-')
+  .option('--cleanup', 'Removes all files in the given targets starting with the given prefix');  
 
 program.parse(process.argv);
 
@@ -29,7 +28,7 @@ if (!program.args.length) {
     process.on('exit', function(code) {
         var time = process.hrtime(global.startTime);
         if(program.debug) {
-            console.log("Overall time needed for execution: " + time[0] + 's ' + time[1] + 'ns');
+            console.log('Overall time needed for execution: ' + time[0] + 's ' + time[1] + 'ns');
         }
     });
 
@@ -43,7 +42,7 @@ if (!program.args.length) {
         }
         program.quiet = program.quiet || false;
 
-        var instrumenter = new Instrumenter(PREFIX, program.verbose, program.quiet, program.debug, program.parallelism);
+        var instrumenter = new Instrumenter(program.prefix, program.verbose, program.quiet, program.debug, program.parallelism);
 
         if (program.cleanup) {
             instrumenter.cleanup(target);
