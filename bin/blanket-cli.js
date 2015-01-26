d2e457ab92cacd1f25b9d1f8669d117b51570203
flower-pot <fbranczyk@gmail.com>
2015-01-26 13:07:30 +0100

    change structure of project
    this increases the testability of the tool

---
+++ bin/blanket-cli.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+Error.stackTraceLimit = Infinity;
+
+var program    = require('commander');
+var os         = require('os');
+var package    = require('../package.json');
+var BlanketCLI = require('../blanket-cli.js');
+
+program
+  .version(package.version)
+  .description('Instrument javascript code for coverage analysis with blanket.js')
+  .usage('[options] [target ...]')
+  .option('-R, --recursive', 'Instrument a directory recursively')
+  .option('-s, --separate [dir]', 'Separate instrumented files in different subdir', "")
+  .option('-d, --debug', 'Display time used for overall processing. If used in combination with --verbose it display time used for each file to instrument', false)
+  .option('-v, --verbose', 'Display some information on the current status')
+  .option('-q, --quiet', 'Surpress warnings and log output')
+  .option('-p, --parallelism <forks>', 'Spread work over n parallel processes (defaults to amount of available cpu cores)', os.cpus().length)
+  .option('--prefix [prefix]', 'The prefix to use to indicate a file is instrumented (by default "instrumented-" or empty when run with -s flag)')
+  .option('--cleanup', 'Removes all files in the given targets starting with the given prefix');  
+
+program.parse(process.argv);
+
+if (!program.args.length) {
+    program.help();
+    process.exit(0);
+}
+
+var targets = program.args;
+var options = program;
+
+new BlanketCLI(targets, options).run();
---
+++ blanket-cli.js
@@ -0,0 +1,63 @@
+var fs           = require('fs');
+var Instrumenter = require('./instrumenter');
+
+const DEFAULT_PREFIX = "instrumented-";
+
+var BlanketCLI = function(targets, options) {
+    var targets = targets;
+    var options = options;
+
+    this.run = function() {
+        process.on('exit', function(code) {
+            var time = process.hrtime(global.startTime);
+            if(options.debug) {
+                console.log('Overall time needed for execution: ' + time[0] + 's ' + time[1] + 'ns');
+            }
+        });
+
+        global.startTime = process.hrtime();
+        targets.forEach(function(target) {
+            try {
+                var stat = fs.statSync(target);
+            } catch(error) {
+                console.log('Omitting ' + target);
+                return;
+            }
+
+            options.quiet = options.quiet || false;
+            if (options.separate && options.prefix === undefined) options.prefix = '';
+            if (options.prefix === undefined) options.prefix = DEFAULT_PREFIX;
+
+            printWorkingParameters(options);
+
+            var instrumenter = new Instrumenter(options.prefix, options.verbose, options.quiet, options.debug, options.parallelism);
+
+            if (options.cleanup) {
+                instrumenter.cleanup(target, options.separate, options.recursive);
+                return;
+            }
+
+            if(stat.isDirectory()) {
+                instrumenter.instrumentDir(target, options.separate, options.recursive);
+            }
+            if(stat.isFile()) {
+                instrumenter.instrumentSingleFile(target);
+            }
+        });
+    };
+};
+
+function printWorkingParameters(program) {
+  for (var option in program.options) {
+    var key = program.options[option].long.substr(2);
+
+    if (key === 'version') continue;
+
+    var value = program[key];
+    if (value === undefined) value = 'not set';
+
+    console.log(key + ': ' + value);
+  }
+}
+
+module.exports = BlanketCLI;
